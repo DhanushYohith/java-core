466ece6f74e2d7e776870d4ddd1aeb281ec45384
DhanushYohith <dkysastra1923@gmail.com>
2024-03-04 23:45:50 +0530

    Day 2 Problem

---
+++ MLA Phase 0 - Practice Exercises/Day 1/src/com/learning/core/day2/D02P01.java
@@ -0,0 +1,5 @@
+package com.learning.core.day2;
+
+public class D02P01 {
+
+}
---
+++ MLA Phase 0 - Practice Exercises/Day 2/D02P02.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class D02P02{
+	//List all even no. less than user ip
+
+	Scanner ip = new Scanner(System.in);
+	int num = ip.nextInt();
+	
+	for(int i=0;i<num;i+=2){
+		System.out.print(i);
+		
+}
+	
+}
